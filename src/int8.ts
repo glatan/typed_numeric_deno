33eab7e1aacf8786e2d014dc0544d68dd1d0c595
glatan <glatan.edu@gmail.com>
2020-05-25 01:14:58 +0900

    add Int8

---
+++ src/int8.ts
@@ -0,0 +1,101 @@
+import { Numeric } from "./mod.ts";
+
+const MAX: number = 0x7F;
+const MIN: number = -0x7F;
+const BIT_LENGTH: number = 8;
+
+export class Int8 implements Numeric<Int8> {
+  #value: number;
+  constructor(value: number) {
+    if (value < 0) {
+      this.#value = ~((~value + 1) & MAX) + 1;
+    } else {
+      this.#value = value & MAX;
+    }
+  }
+  value(): number {
+    return this.#value;
+  }
+  max(): number {
+    return MAX;
+  }
+  min(): number {
+    return MIN;
+  }
+  add(value: Int8): Int8 {
+    return new Int8(this.#value + value.#value);
+  }
+  sub(value: Int8): Int8 {
+    return new Int8(this.#value - value.#value);
+  }
+  div(value: Int8): Int8 {
+    return new Int8(this.#value / value.#value);
+  }
+  mul(value: Int8): Int8 {
+    return new Int8(this.#value * value.#value);
+  }
+  rem(value: Int8): Int8 {
+    return new Int8(this.#value % value.#value);
+  }
+  exp(value: Int8): Int8 {
+    return new Int8(this.#value ** value.#value);
+  }
+  and(value: Int8): Int8 {
+    return new Int8(this.#value & value.#value);
+  }
+  or(value: Int8): Int8 {
+    return new Int8(this.#value | value.#value);
+  }
+  xor(value: Int8): Int8 {
+    return new Int8(this.#value ^ value.#value);
+  }
+  not(): Int8 {
+    return new Int8(~this.#value);
+  }
+  logicalLeft(n: number): Int8 {
+    if (n >= BIT_LENGTH) {
+      return new Int8(0);
+    }
+    return new Int8(this.#value << n);
+  }
+  logicalRight(n: number): Int8 {
+    if (n >= BIT_LENGTH) {
+      return new Int8(0);
+    }
+    return new Int8(this.#value >> n);
+  }
+  rotateLeft(n: number): Int8 {
+    return new Int8(
+      (this.#value << (n % BIT_LENGTH)) |
+        (this.#value >> ((BIT_LENGTH - n) % BIT_LENGTH)),
+    );
+  }
+  rotateRight(n: number): Int8 {
+    return new Int8(
+      (this.#value >> (n % BIT_LENGTH)) |
+        (this.#value << ((BIT_LENGTH - n) % BIT_LENGTH)),
+    );
+  }
+  fromBeBytes(bytes: Uint8Array): Int8 {
+    if (bytes.length === (BIT_LENGTH / 8)) {
+      return new Int8(bytes[0]);
+    }
+    throw new Error(
+      "Invalid Length Error: Expected Uint8Array.prototype.length is 1",
+    );
+  }
+  fromLeBytes(bytes: Uint8Array): Int8 {
+    if (bytes.length === (BIT_LENGTH / 8)) {
+      return new Int8(bytes[0]);
+    }
+    throw new Error(
+      "Invalid Length Error: Expected Uint8Array.prototype.length is 1",
+    );
+  }
+  toBeBytes(): Uint8Array {
+    return Uint8Array.from([this.#value]);
+  }
+  toLeBytes(): Uint8Array {
+    return Uint8Array.from([this.#value]);
+  }
+}
