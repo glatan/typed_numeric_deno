6d1d09196e90f7c248fcc24582b596cde4bf4fe1
glatan <glatan.edu@gmail.com>
2020-05-26 18:57:51 +0900

    add Int64

---
+++ src/int64.ts
@@ -0,0 +1,233 @@
+import { Numeric } from "./mod.ts";
+
+const MAX: bigint = 0x7FFFFFFF_FFFFFFFFn;
+const MIN: bigint = -0x7FFFFFFF_FFFFFFFFn;
+const BIT_LENGTH: bigint = 64n;
+
+export class Int64 implements Numeric<Int64> {
+  #value: bigint;
+  constructor(value: bigint) {
+    if (value < 0) {
+      // 一度符号を外してからマスク、その後符号を(Int64の最上位ビットを1にする形で)戻す
+      this.#value = ((~value + 1n) & MAX) | 0x80000000_00000000n;
+    } else if (value === (value | 0x80000000_00000000n)) {
+      // Int64での最上位ビットが1の場合
+      this.#value = value & 0xFFFFFFFF_FFFFFFFFn;
+    } else {
+      this.#value = value & MAX;
+    }
+  }
+  value(): bigint {
+    if ((this.#value | 0x80000000_00000000n) === this.#value) {
+      // Int64での最上位ビットが1の場合
+      return ~(this.#value & 0x7FFFFFFF_FFFFFFFFn) + 1n;
+    } else {
+      return this.#value;
+    }
+  }
+  max(): bigint {
+    return MAX;
+  }
+  min(): bigint {
+    return MIN;
+  }
+  add(value: Int64): Int64 {
+    if (
+      this.#value === (this.#value | 0x80000000_00000000n) &&
+      value.#value === (value.#value | 0x80000000_00000000n)
+    ) {
+      // -Num + -Num
+      return new Int64(
+        ~(this.#value & 0x7FFFFFFF_FFFFFFFFn) +
+          ~(value.#value & 0x7FFFFFFF_FFFFFFFFn) + 2n,
+      );
+    } else if (this.#value === (this.#value | 0x80000000_00000000n)) {
+      // -Num + Num
+      return new Int64(
+        value.#value + ~(this.#value & 0x7FFFFFFF_FFFFFFFFn) + 1n,
+      );
+    } else if (value.#value === (value.#value | 0x80000000_00000000n)) {
+      // Num + -Num
+      return new Int64(
+        this.#value + ~(value.#value & 0x7FFFFFFF_FFFFFFFFn) + 1n,
+      );
+    } else {
+      // Num + Num
+      return new Int64(this.#value + value.#value);
+    }
+  }
+  sub(value: Int64): Int64 {
+    if (
+      // -Num - -Num
+      this.#value === (this.#value | 0x80000000_00000000n) &&
+      value.#value === (value.#value | 0x80000000_00000000n)
+    ) {
+      if (this.#value < value.#value) {
+        // -Num + Num
+        return new Int64(
+          ~(this.#value & 0x7FFFFFFF_FFFFFFFFn) +
+            (value.#value & 0x7FFFFFFF_FFFFFFFFn) + 1n,
+        );
+      } else {
+        return new Int64(
+          ~(this.#value & 0x7FFFFFFF_FFFFFFFFn) +
+            ~(value.#value & 0x7FFFFFFF_FFFFFFFFn) + 2n,
+        );
+      }
+    } else if (this.#value === (this.#value | 0x80000000_00000000n)) {
+      // -Num - Num
+      return new Int64(
+        ~(this.#value & 0x7FFFFFFF_FFFFFFFFn) +
+          ~(value.#value & 0x7FFFFFFF_FFFFFFFFn) + 2n,
+      );
+    } else if (value.#value === (value.#value | 0x80000000_00000000n)) {
+      // Num - -Num
+      return new Int64(this.#value + (value.#value & 0x7FFFFFFF_FFFFFFFFn));
+    } else {
+      // Num - Num
+      return new Int64(this.#value + ~value.#value + 1n);
+    }
+  }
+  div(value: Int64): Int64 {
+    if (
+      this.#value === (this.#value | 0x80000000_00000000n) &&
+      value.#value === (value.#value | 0x80000000_00000000n)
+    ) {
+      return new Int64(
+        (~(this.#value & 0x7FFFFFFF_FFFFFFFFn) + 1n) /
+          (~(value.#value & 0x7FFFFFFF_FFFFFFFFn) + 1n),
+      );
+    } else if (this.#value === (this.#value | 0x80000000_00000000n)) {
+      return new Int64(
+        ~((this.#value & 0x7FFFFFFF_FFFFFFFFn) / value.#value) + 1n,
+      );
+    } else if (value.#value === (value.#value | 0x80000000_00000000n)) {
+      return new Int64(
+        ~(this.#value / (value.#value & 0x7FFFFFFF_FFFFFFFFn)) + 1n,
+      );
+    } else {
+      return new Int64(this.#value / value.#value);
+    }
+  }
+  mul(value: Int64): Int64 {
+    if (
+      this.#value === (this.#value | 0x80000000_00000000n) &&
+      value.#value === (value.#value | 0x80000000_00000000n)
+    ) {
+      return new Int64(
+        (~(this.#value & 0x7FFFFFFF_FFFFFFFFn) + 1n) *
+          (~(value.#value & 0x7FFFFFFF_FFFFFFFFn) + 1n),
+      );
+    } else if (this.#value === (this.#value | 0x80000000_00000000n)) {
+      return new Int64(
+        ~((this.#value & 0x7FFFFFFF_FFFFFFFFn) * value.#value) + 1n,
+      );
+    } else if (value.#value === (value.#value | 0x80000000_00000000n)) {
+      return new Int64(
+        ~(this.#value * (value.#value & 0x7FFFFFFF_FFFFFFFFn)) + 1n,
+      );
+    } else {
+      return new Int64(this.#value * value.#value);
+    }
+  }
+  rem(value: Int64): Int64 {
+    return new Int64(this.#value % value.#value);
+  }
+  exp(value: Int64): Int64 {
+    return new Int64(this.#value ** value.#value);
+  }
+  and(value: Int64): Int64 {
+    return new Int64(this.#value & value.#value);
+  }
+  or(value: Int64): Int64 {
+    return new Int64(this.#value | value.#value);
+  }
+  xor(value: Int64): Int64 {
+    return new Int64(this.#value ^ value.#value);
+  }
+  not(): Int64 {
+    return new Int64(~this.#value);
+  }
+  logicalLeft(n: bigint): Int64 {
+    if (n >= BIT_LENGTH) {
+      return new Int64(0n);
+    }
+    return new Int64(this.#value << n);
+  }
+  logicalRight(n: bigint): Int64 {
+    if (n >= BIT_LENGTH) {
+      return new Int64(0n);
+    }
+    return new Int64(this.#value >> n);
+  }
+  rotateLeft(n: bigint): Int64 {
+    return new Int64(
+      (this.#value << (n % BIT_LENGTH)) |
+        (this.#value >> ((BIT_LENGTH - n) % BIT_LENGTH)),
+    );
+  }
+  rotateRight(n: bigint): Int64 {
+    return new Int64(
+      (this.#value >> (n % BIT_LENGTH)) |
+        (this.#value << ((BIT_LENGTH - n) % BIT_LENGTH)),
+    );
+  }
+  fromBeBytes(bytes: Uint8Array): Int64 {
+    if (bytes.length === 8) {
+      return new Int64(
+        ((BigInt(bytes[0]) << 56n) & 0xFF000000_00000000n) |
+          ((BigInt(bytes[1]) << 48n) & 0xFF0000_00000000n) |
+          ((BigInt(bytes[2]) << 40n) & 0xFF00_00000000n) |
+          ((BigInt(bytes[3]) << 32n) & 0xFF_00000000n) |
+          ((BigInt(bytes[4]) << 24n) & 0xFF000000n) |
+          ((BigInt(bytes[5]) << 16n) & 0xFF0000n) |
+          ((BigInt(bytes[6]) << 8n) & 0xFF00n) |
+          (BigInt(bytes[7]) & 0xFFn),
+      );
+    }
+    throw new Error(
+      "Invalid Length Error: Expected Uint8Array.prototype.length is 8",
+    );
+  }
+  fromLeBytes(bytes: Uint8Array): Int64 {
+    if (bytes.length === 8) {
+      return new Int64(
+        ((BigInt(bytes[7]) << 56n) & 0xFF000000_00000000n) |
+          ((BigInt(bytes[6]) << 48n) & 0xFF0000_00000000n) |
+          ((BigInt(bytes[5]) << 40n) & 0xFF00_00000000n) |
+          ((BigInt(bytes[4]) << 32n) & 0xFF_00000000n) |
+          ((BigInt(bytes[3]) << 24n) & 0xFF00_0000n) |
+          ((BigInt(bytes[2]) << 16n) & 0xFF_0000n) |
+          ((BigInt(bytes[1]) << 8n) & 0xFF00n) |
+          (BigInt(bytes[0]) & 0xFFn),
+      );
+    }
+    throw new Error(
+      "Invalid Length Error: Expected Uint8Array.prototype.length is 8",
+    );
+  }
+  toBeBytes(): Uint8Array {
+    return Uint8Array.from([
+      Number((this.#value >> 56n) & 0xFFn),
+      Number((this.#value >> 48n) & 0xFFn),
+      Number((this.#value >> 40n) & 0xFFn),
+      Number((this.#value >> 32n) & 0xFFn),
+      Number((this.#value >> 24n) & 0xFFn),
+      Number((this.#value >> 16n) & 0xFFn),
+      Number((this.#value >> 8n) & 0xFFn),
+      Number(this.#value & 0xFFn),
+    ]);
+  }
+  toLeBytes(): Uint8Array {
+    return Uint8Array.from([
+      Number(this.#value & 0xFFn),
+      Number((this.#value >> 8n) & 0xFFn),
+      Number((this.#value >> 16n) & 0xFFn),
+      Number((this.#value >> 24n) & 0xFFn),
+      Number((this.#value >> 32n) & 0xFFn),
+      Number((this.#value >> 40n) & 0xFFn),
+      Number((this.#value >> 48n) & 0xFFn),
+      Number((this.#value >> 56n) & 0xFFn),
+    ]);
+  }
+}
---
+++ src/int64_test.ts
@@ -0,0 +1,291 @@
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@0.52.0/testing/asserts.ts";
+
+import { Int64 } from "./int64.ts";
+
+Deno.test("Int64", () => {
+  // value()
+  assertEquals(new Int64(0xFFFFFFFF_FFFFFFFFn).value(), -0x7FFFFFFF_FFFFFFFFn);
+  assertEquals(new Int64(-0xFFFFFFFF_FFFFFFFFn).value(), -0x7FFFFFFF_FFFFFFFFn);
+  assertEquals(new Int64(0n).value(), 0n);
+  assertEquals(new Int64(-0n).value(), 0n);
+  assertEquals(new Int64(0x80000000_00000000n).value(), 0n);
+  assertEquals(new Int64(-0x80000000_00000000n).value(), 0n);
+  // max()
+  assertEquals(Int64.prototype.max(), 0x7FFFFFFF_FFFFFFFFn);
+  // min()
+  assertEquals(Int64.prototype.min(), -0x7FFFFFFF_FFFFFFFFn);
+  // add()
+  assertEquals(new Int64(1n).add(new Int64(2n)).value(), 3n);
+  assertEquals(new Int64(0x7FFFFFFF_FFFFFFFFn).add(new Int64(1n)).value(), 0n);
+  assertEquals(new Int64(1n).add(new Int64(-2n)).value(), -1n);
+  assertEquals(new Int64(-1n).add(new Int64(2n)).value(), 1n);
+  assertEquals(new Int64(-1n).add(new Int64(-2n)).value(), -3n);
+  // sub()
+  assertEquals(new Int64(3n).sub(new Int64(2n)).value(), 1n);
+  assertEquals(new Int64(0n).sub(new Int64(1n)).value(), -1n);
+  assertEquals(new Int64(1n).sub(new Int64(-2n)).value(), 3n);
+  assertEquals(new Int64(-2n).sub(new Int64(1n)).value(), -3n);
+  assertEquals(new Int64(-1n).sub(new Int64(-2n)).value(), 1n);
+  // div()
+  assertEquals(new Int64(2n).div(new Int64(3n)).value(), 0n);
+  assertEquals(new Int64(3n).div(new Int64(2n)).value(), 1n);
+  assertEquals(new Int64(4n).div(new Int64(-2n)).value(), -2n);
+  assertEquals(new Int64(-4n).div(new Int64(2n)).value(), -2n);
+  assertEquals(new Int64(-4n).div(new Int64(-2n)).value(), 2n);
+  // mul()
+  assertEquals(new Int64(1n).mul(new Int64(2n)).value(), 2n);
+  assertEquals(new Int64(1n).mul(new Int64(0n)).value(), 0n);
+  assertEquals(new Int64(1n).mul(new Int64(-2n)).value(), -2n);
+  assertEquals(new Int64(-1n).mul(new Int64(2n)).value(), -2n);
+  assertEquals(new Int64(-1n).mul(new Int64(-2n)).value(), 2n);
+  // rem()
+  assertEquals(new Int64(2n).rem(new Int64(3n)).value(), 2n);
+  assertEquals(new Int64(3n).rem(new Int64(2n)).value(), 1n);
+  // exp()
+  assertEquals(new Int64(2n).exp(new Int64(3n)).value(), 8n);
+  assertEquals(
+    new Int64(0x7FFFFFFF_FFFFFFFFn).exp(new Int64(1n)).value(),
+    0x7FFFFFFF_FFFFFFFFn,
+  );
+  assertEquals(new Int64(0x7FFFFFFF_FFFFFFFFn).exp(new Int64(0n)).value(), 1n);
+  assertThrows((): void => {
+    new Int64(0x7FFFFFFF_FFFFFFFFn).exp(new Int64(0x7FFFFFFF_FFFFFFFFn));
+  });
+  // and()
+  assertEquals(new Int64(0n).and(new Int64(0n)).value(), 0n);
+  assertEquals(new Int64(0x7FFFFFFF_FFFFFFFFn).and(new Int64(0n)).value(), 0n);
+  assertEquals(
+    new Int64(0x7FFFFFFF_FFFFFFFFn).and(new Int64(0x7FFFFFFF_FFFFFFFFn))
+      .value(),
+    0x7FFFFFFF_FFFFFFFFn,
+  );
+  // or()
+  assertEquals(new Int64(0n).or(new Int64(0n)).value(), 0n);
+  assertEquals(
+    new Int64(0x7FFFFFFF_FFFFFFFFn).or(new Int64(0n)).value(),
+    0x7FFFFFFF_FFFFFFFFn,
+  );
+  assertEquals(
+    new Int64(0x7FFFFFFF_FFFFFFFFn).or(new Int64(0x7FFFFFFF_FFFFFFFFn)).value(),
+    0x7FFFFFFF_FFFFFFFFn,
+  );
+  // xor()
+  assertEquals(new Int64(0n).xor(new Int64(0n)).value(), 0n);
+  assertEquals(
+    new Int64(0x7FFFFFFF_FFFFFFFFn).xor(new Int64(0n)).value(),
+    0x7FFFFFFF_FFFFFFFFn,
+  );
+  assertEquals(
+    new Int64(0x7FFFFFFF_FFFFFFFFn).xor(new Int64(0x7FFFFFFF_FFFFFFFFn))
+      .value(),
+    0n,
+  );
+  // not()
+  assertEquals(new Int64(0n).not().value(), -1n);
+  assertEquals(new Int64(0x7FFFFFFF_FFFFFFFFn).not().value(), 0n);
+  // logicalLeft()
+  assertEquals(
+    new Int64(0x12345678_90123456n).logicalLeft(0n).value(),
+    0x12345678_90123456n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).logicalLeft(32n).value(),
+    -0x10123456_00000000n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).logicalLeft(64n).value(),
+    0n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).logicalLeft(128n).value(),
+    0n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).logicalLeft(256n).value(),
+    0n,
+  );
+  assertEquals(
+    new Int64(-0x12345678_90123456n).logicalLeft(0n).value(),
+    -0x12345678_90123456n,
+  );
+  assertEquals(
+    new Int64(-0x12345678_90123456n).logicalLeft(32n).value(),
+    -0x10123456_00000000n,
+  );
+  assertEquals(
+    new Int64(-0x12345678_90123456n).logicalLeft(33n).value(),
+    0x202468AC_00000000n,
+  );
+  assertEquals(new Int64(-0x12345678_90123456n).logicalLeft(64n).value(), 0n);
+  assertEquals(new Int64(-0x12345678_90123456n).logicalLeft(128n).value(), 0n);
+  assertEquals(new Int64(-0x12345678_90123456n).logicalLeft(256n).value(), 0n);
+  assertEquals(
+    new Int64(0x7FFFFFFF_FFFFFFFFn).logicalLeft(1n).value(),
+    -0x7FFFFFFF_FFFFFFFEn,
+  );
+  assertEquals(
+    new Int64(-0x7FFFFFFF_FFFFFFFFn).logicalLeft(1n).value(),
+    -0x7FFFFFFF_FFFFFFFEn,
+  );
+  // logicalRight()
+  assertEquals(
+    new Int64(0x12345678_90123456n).logicalRight(0n).value(),
+    0x12345678_90123456n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).logicalRight(32n).value(),
+    0x00000000_12345678n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).logicalRight(64n).value(),
+    0n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).logicalRight(128n).value(),
+    0n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).logicalRight(256n).value(),
+    0n,
+  );
+  assertEquals(
+    new Int64(-0x12345678_90123456n).logicalRight(0n).value(),
+    -0x12345678_90123456n,
+  );
+  assertEquals(
+    new Int64(-0x12345678_90123456n).logicalRight(32n).value(),
+    0x92345678n,
+  );
+  assertEquals(
+    new Int64(-0x12345678_90123456n).logicalRight(33n).value(),
+    0x491A2B3Cn,
+  );
+  assertEquals(new Int64(-0x12345678_90123456n).logicalRight(64n).value(), 0n);
+  assertEquals(new Int64(-0x12345678_90123456n).logicalRight(128n).value(), 0n);
+  assertEquals(new Int64(-0x12345678_90123456n).logicalRight(256n).value(), 0n);
+  assertEquals(
+    new Int64(0x7FFFFFFF_FFFFFFFFn).logicalRight(1n).value(),
+    0x3FFFFFFF_FFFFFFFFn,
+  );
+  assertEquals(
+    new Int64(-0x7FFFFFFF_FFFFFFFFn).logicalRight(1n).value(),
+    0x7FFFFFFF_FFFFFFFFn,
+  );
+  // rotateLeft()
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateLeft(0n).value(),
+    0x12345678_90123456n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateLeft(16n).value(),
+    0x56789012_34561234n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateLeft(32n).value(),
+    -0x10123456_12345678n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateLeft(64n).value(),
+    0x12345678_90123456n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateLeft(128n).value(),
+    0x12345678_90123456n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateLeft(256n).value(),
+    0x12345678_90123456n,
+  );
+  // rotateRight()
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateRight(0n).value(),
+    0x12345678_90123456n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateRight(16n).value(),
+    0x34561234_56789012n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateRight(32n).value(),
+    -0x10123456_12345678n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateRight(64n).value(),
+    0x12345678_90123456n,
+  );
+  assertEquals(
+    new Int64(0x12345678_90123456n).rotateRight(128n).value(),
+    0x12345678_90123456n,
+  );
+  // fromBeBytes()
+  assertEquals(
+    Int64.prototype.fromBeBytes(
+      Uint8Array.from([0x12, 0x34, 0x56, 0x78, 0x90, 0x12, 0x34, 0x56]),
+    ).value(),
+    new Int64(0x12345678_90123456n).value(),
+  );
+  assertEquals(
+    Int64.prototype.fromBeBytes(
+      new Uint8Array([0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]),
+    ).value(),
+    Int64.prototype.max(),
+  );
+  assertEquals(
+    Int64.prototype.fromBeBytes(new Uint8Array(8)).value(),
+    0n,
+  );
+  assertThrows((): void => {
+    // Invalid Length
+    Int64.prototype.fromBeBytes(new Uint8Array(9));
+  });
+  // fromLeBytes()
+  assertEquals(
+    Int64.prototype.fromLeBytes(
+      Uint8Array.from([0x12, 0x34, 0x56, 0x78, 0x90, 0x12, 0x34, 0x56]),
+    ).value(),
+    new Int64(0x56341290_78563412n).value(),
+  );
+  assertEquals(
+    Int64.prototype.fromLeBytes(
+      new Uint8Array([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F]),
+    ).value(),
+    Int64.prototype.max(),
+  );
+  assertEquals(
+    Int64.prototype.fromLeBytes(new Uint8Array(8)).value(),
+    0n,
+  );
+  assertThrows((): void => {
+    // Invalid Length
+    Int64.prototype.fromLeBytes(new Uint8Array(9));
+  });
+  // toBeBytes()
+  assertEquals(
+    new Int64(0x12345678_90123456n).toBeBytes(),
+    new Uint8Array([0x12, 0x34, 0x56, 0x78, 0x90, 0x12, 0x34, 0x56]),
+  );
+  assertEquals(
+    new Int64(Int64.prototype.max()).toBeBytes(),
+    new Uint8Array([0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]),
+  );
+  assertEquals(
+    new Int64(0n).toBeBytes(),
+    new Uint8Array(8),
+  );
+  // toLeBytes()
+  assertEquals(
+    new Int64(0x12345678_90123456n).toLeBytes(),
+    new Uint8Array([0x56, 0x34, 0x12, 0x90, 0x78, 0x56, 0x34, 0x12]),
+  );
+  assertEquals(
+    new Int64(Int64.prototype.max()).toLeBytes(),
+    new Uint8Array([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F]),
+  );
+  assertEquals(
+    new Int64(0n).toLeBytes(),
+    new Uint8Array(8),
+  );
+});
