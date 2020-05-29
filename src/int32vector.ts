f461345349cfc1048eacb35769492d23d2374c55
glatan <glatan.edu@gmail.com>
2020-05-30 00:43:08 +0900

    Add Int32Vector

---
+++ src/int32vector.ts
@@ -0,0 +1,33 @@
+import { Int32 } from "./int32.ts";
+import { Vector } from "./mod.ts";
+
+export class Int32Vector extends Vector<Int32> {
+  constructor(arg: number | Array<Int32>) {
+    if (typeof arg === "number") {
+      super(new Array(arg).fill(new Int32(0n)));
+    }
+    if (arg instanceof Array) {
+      super(arg as Array<Int32>);
+    }
+  }
+  fill(value: Int32): Int32Vector {
+    for (let i = 0; i < this.inner.length; i++) {
+      this.inner[i] = value;
+    }
+    return new Int32Vector(this.inner);
+  }
+  toTypedArray(): Int32Array {
+    let array = new Int32Array(this.inner.length);
+    for (let i = 0; i < this.inner.length; i++) {
+      array[i] = Number(this.inner[i].value());
+    }
+    return array;
+  }
+  static fromTypedArray(array: Int32Array): Int32Vector {
+    const vector = new Int32Vector(0);
+    for (let i = 0; i < array.length; i++) {
+      vector.push(new Int32(BigInt(array[i])));
+    }
+    return vector;
+  }
+}
---
+++ src/int32vector_test.ts
@@ -0,0 +1,84 @@
+import { assertEquals, assertThrows } from "../depends.ts";
+
+import { Int32Vector } from "./int32vector.ts";
+import { Int32 } from "./int32.ts";
+
+Deno.test("Int32Vector", () => {
+  const vec = new Int32Vector(2);
+  assertEquals(vec.length, 2);
+  // Int32Vector from Array<Int32>
+  assertEquals(
+    new Int32Vector(Array.from([new Int32(Int32.max())])).value_by_index(0)
+      .value(),
+    Int32.max(),
+  );
+  // value_by_index
+  assertEquals(vec.value_by_index(0).value(), 0n);
+  assertEquals(vec.value_by_index(1).value(), 0n);
+  assertThrows(() => {
+    // Index out of range
+    vec.value_by_index(2);
+  });
+  assertThrows(() => {
+    // This Vector<T> is empty.
+    new Int32Vector(0).value_by_index(0);
+  });
+  // push
+  vec.push(new Int32(0n));
+  assertEquals(vec.length, 3);
+  // pop
+  assertEquals(vec.pop().value(), 0n);
+  assertEquals(vec.length, 2);
+  assertEquals(vec.pop().value(), 0n);
+  assertEquals(vec.length, 1);
+  assertEquals(vec.pop().value(), 0n);
+  assertEquals(vec.length, 0);
+  assertThrows(() => {
+    // This Vector<T> is empty.
+    vec.pop();
+  });
+  assertEquals(vec.length, 0);
+  // concat
+  vec.concat(new Int32Vector(3));
+  assertEquals(vec.length, 3);
+  // toTypedArray
+  assertEquals(vec.toTypedArray(), new Int32Array(3));
+  assertEquals(
+    new Int32Vector(5).fill(new Int32(Int32.max())).toTypedArray(),
+    new Int32Array(5).fill(Number(Int32.max())),
+  );
+  // fill
+  vec.fill(new Int32(Int32.max()));
+  assertEquals(
+    vec.toTypedArray(),
+    new Int32Array(3).fill(Number(Int32.max())),
+  );
+  vec.fill(new Int32(Int32.min()));
+  assertEquals(vec.toTypedArray(), new Int32Array(3).fill(Number(Int32.min())));
+  // fromTypedArray and equals
+  assertEquals(
+    Int32Vector.fromTypedArray(new Int32Array(3)).equals(new Int32Vector(3)),
+    true,
+  );
+  assertEquals(
+    Int32Vector.fromTypedArray(new Int32Array(3)).equals(vec),
+    false,
+  );
+  assertEquals(
+    Int32Vector.fromTypedArray(new Int32Array(3)).fill(
+      new Int32(Int32.max()),
+    ).equals(vec),
+    false,
+  );
+  assertEquals(
+    Int32Vector.fromTypedArray(
+      new Int32Array(5).fill(Number(Int32.max())),
+    ).equals(new Int32Vector(5).fill(new Int32(Int32.max()))),
+    true,
+  );
+  // slice
+  assertEquals(
+    new Int32Vector(5).slice(0, 3).equals(new Int32Vector(3)),
+    true,
+  );
+});
