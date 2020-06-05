c8217dcd98e5b96dd61a549cf0cfbae476c8e8fb
glatan <glatan.edu@gmail.com>
2020-06-06 02:54:48 +0900

    test: add jsdoc style example

--- src/numeric/uint8.ts
+++ src/numeric/uint8.ts
@@ -9,6 +9,16 @@ export class Uint8 extends Numeric<Uint8, number> {
   constructor(value: number = 0) {
     super(value & MAX);
   }
+  /**
+   * @example
+   * import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+   * import { Uint8 } from "https://deno.land/x/typed_numeric/mod.ts";
+   * 
+   * assertEquals(new Uint8(100).value(), 100);
+   * assertEquals(new Uint8(0xFF).value(), 255);
+   * assertEquals(new Uint8(0x100).value(), 0);
+   * assertEquals(new Uint8(0xFFFF).value(), 255);
+   */
   value(): number {
     return this.inner;
   }
